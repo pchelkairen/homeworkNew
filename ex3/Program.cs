16b260458ff8eddc0e6f322fb479476f53297627
Irina <pamir.08@mail.ru>
2022-10-11 12:01:28 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите четверть: ");
+int a = int.Parse (Console.ReadLine ());
+
+if (a == 1)
+{
+    Console.WriteLine ("Диапазон возможных координат x>0 и y>0");
+}
+
+else if (a == 2)
+{
+   Console.WriteLine ("Диапазон возможных координат x<0 и y>0"); 
+}
+
+else if (a == 3)
+{
+   Console.WriteLine ("Диапазон возможных координат x<0 и y<0"); 
+}
+
+else
+{
+   Console.WriteLine ("Диапазон возможных координат x>0 и y<0"); 
+}
