a03c8a828f77fb370d67abcf7f712f3ffb35c7cc
Irina <pamir.08@mail.ru>
2022-10-11 11:03:46 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Ведите число от 1 до 7: ");
+int a = int.Parse (Console.ReadLine ());
+
+if (a >= 6)
+{
+    Console.WriteLine ("Номер дня недели является выходным");
+}
+else
+{
+Console.WriteLine ("Номер дня недели не является выходным");
+}
