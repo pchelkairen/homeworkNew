9cdac514f64440d59eda808ffda70efee187d0c0
Irina <pamir.08@mail.ru>
2022-10-11 12:37:41 +0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Введите число x1: ");
+int x1 = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число y1: ");
+int y1 = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число x2: ");
+int x2 = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число y2: ");
+int y2 = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число z1: ");
+int z1 = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число z2: ");
+int z2 = int.Parse (Console.ReadLine ());
+
+double a = Math.Sqrt (Math.Pow ((x2 - x1), 2) + Math.Pow ((y2 - y1), 2) + Math.Pow ((z2 - z1), 2));
+
+Console.Write ("Pасстояние между точками в пространстве ");
+Console.Write (a);
