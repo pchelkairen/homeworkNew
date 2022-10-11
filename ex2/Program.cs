c517463edabcd9eab294d303f2c2646c7e47e66c
Irina <pamir.08@mail.ru>
2022-10-11 11:39:19 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите число a: ");
+int a = int.Parse (Console.ReadLine ());
+
+Console.WriteLine ("Введите число b: ");
+int b = int.Parse (Console.ReadLine ());
+
+if (a == b * b)
+{
+    Console.WriteLine ("Число а является квадратом числа b");
+}
+
+else if (b == a * a)
+{
+    Console.WriteLine ("Число b является квадратом числа a");
+}
+
+else
+{
+    Console.WriteLine ("Одно число не является квадратом другого");
+}
